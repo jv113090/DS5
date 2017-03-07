cd0e74898aa402d42c7bbdb7f73fbe3ce0e3de76
jv113090 <jv113090@gmail.com>
2017-03-06 17:32:20 -0800

    Add files via upload

--- DSProject.py
+++ DSProject.py
@@ -8,33 +8,44 @@ Created on Tue Jan 31 20:10:02 2017
 #import data
 
 import pandas as pd
-url = '../Project/ProjData.csv'
-Data = pd.read_csv(url, sep=' ')
+url = '../Project/FinalProject/FinalProjData.csv'
+Data = pd.read_csv(url, index_col=0)
 
-print Data.head()
+Data.head()
 
-# Add in code about dimensionality reduction
+#Convert string to categories 
 
-# K-means with 3 clusters
+Data['MetalLevel_2014'] = Data.MetalLevel_2014.map({'Low':0, 'High':1})
+Data.head()
+
+# create feature matrix (X)
+
+feature_cols = ['State_Number', 'MetalLevel_2014', 'ChildAdultOnly_2014', 'Income', 'PlanID']
+X = Data[feature_cols]
+print(X.shape)
+
+# Add in code about dimensionality reduction if time
+
+# K-means with 2 clusters
 from sklearn.cluster import KMeans
-km = KMeans(n_clusters=3, random_state=1)
-km.fit(Data)
+km = KMeans(n_clusters=2, random_state=1)
+km.fit(X)
 
 # review the cluster labels
 km.labels_
 
 # save the cluster labels and sort by cluster
-Data['cluster'] = km.labels_
-Data.sort_values('ZipCode')
+X['cluster'] = km.labels_
+Data.sort_values('Income')
 
 # review the cluster centers
 km.cluster_centers_
 
 # calculate the mean of each feature for each cluster
-Data.groupby('cluster').mean()
+X.groupby('cluster').mean()
 
 # save the DataFrame of cluster centers
-centers = Data.groupby('cluster').mean()
+centers = X.groupby('cluster').mean()
 
 type(centers)
 
@@ -47,37 +58,35 @@ plt.rcParams['font.size'] = 14
 import numpy as np
 colors = np.array(['red', 'green', 'blue', 'yellow'])
 
-# scatter plot of calories versus alcohol, colored by cluster (0=red, 1=green, 2=blue)
-plt.scatter(beer.ZipCode, beer.Income, c=colors[beer.cluster], s=50)
+# scatter plot of PlanID versus Income, colored by cluster (0=red, 1=green, 2=blue)
+plt.scatter(X.PlanID, X.Income, c=colors[X.cluster], s=50)
 
 # cluster centers, marked by "+"
-plt.scatter(centers.ZipCode, centers.Income, linewidths=3, marker='+', s=300, c='black')
+plt.scatter(centers.PlanID, centers.Income, linewidths=3, marker='+', s=300, c='black')
 
 # add labels
-plt.xlabel('Income')
-plt.ylabel('ZipCode')
+plt.xlabel('PlanID')
+plt.ylabel('Income')
 
 # scatter plot matrix (0=red, 1=green, 2=blue)
-pd.scatter_matrix(X, c=colors[beer.cluster], figsize=(10,10), s=100)
+pd.scatter_matrix(X, c=colors[X.cluster], figsize=(10,10), s=100)
 
 
 
 ###Evaluating the model
 
 
-
-
 # calculate Silhouette Coefficient for K=3
 from sklearn import metrics
-metrics.silhouette_score(X_scaled, km.labels_)
+metrics.silhouette_score(X, km.labels_)
 
-# calculate SC for K=2 through K=19
-k_range = range(2, 20)
+# calculate SC for K=2 through K=5
+k_range = range(2, 5)
 scores = []
 for k in k_range:
     km = KMeans(n_clusters=k, random_state=1)
-    km.fit(X_scaled)
-    scores.append(metrics.silhouette_score(X_scaled, km.labels_))
+    km.fit(X)
+    scores.append(metrics.silhouette_score(X, km.labels_))
     
 # plot the results
 plt.plot(k_range, scores)
@@ -85,11 +94,6 @@ plt.xlabel('Number of clusters')
 plt.ylabel('Silhouette Coefficient')
 plt.grid(True)
 
-# K-means with 4 clusters on scaled data
-km = KMeans(n_clusters=4, random_state=1)
-km.fit(X_scaled)
-Data['cluster'] = km.labels_
-Data.sort_values('cluster')
 
 
 
